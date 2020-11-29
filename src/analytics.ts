791a712e55d9e07ce104accbc8de18dd3292edc5
Harry Shapiro <harrydshapiro96@gmail.com>
2020-11-29 17:02:21 -0500

    added google analytics to this bad boy! sha bang!

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,8 +1,31 @@
 import LogRocket from 'logrocket'
 import api from './api'
 
-export function initAnalytics () {
+function initLogRocket () {
   LogRocket.init('awdeo-1738/prod')
+}
+
+declare global {
+  interface Window { dataLayer: any; }
+}
+
+window.dataLayer = window.dataLayer || {}
+
+function initGA () {
+  const gaScriptTag = document.createElement('script')
+  gaScriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-KGERYQRF9N'
+  gaScriptTag.async = true
+  window.dataLayer = window.dataLayer || []
+  function gtag () { window.dataLayer.push(arguments) }
+  // @ts-ignore
+  gtag('js', new Date())
+  // @ts-ignore
+  gtag('config', 'G-KGERYQRF9N')
+}
+
+export function initAnalytics () {
+  initLogRocket()
+  initGA()
 }
 
 export async function identifyUser () {
